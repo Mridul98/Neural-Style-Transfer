4b15092606396f8ef476b6890571996816c87841
mridul <munmridul@gmail.com>
2024-12-31 02:06:21 +0600

    added minio client api

--- src/api/services.py
+++ src/api/services.py
@@ -1,33 +1,50 @@
 import os
 import shutil
+import logging
 from abc import ABC, abstractmethod
+from minio import Minio
 from fastapi import UploadFile
 
 
 class StorageService(ABC):
 
     @abstractmethod
+    def get_file():
+        raise NotImplementedError()
+    
+    @abstractmethod
     def upload_file():
-        pass
+        raise NotImplementedError()
 
+class MinioStorageService(StorageService):
 
-class LocalStorageService(StorageService):
+    def __init__(self,host:str,access_key:str,secret_key:str):
+       
+        self.client_instance = Minio(
+            host,
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=False
+        )
+        logging.info(f'minio client instance id: {id(self.client_instance)}')
 
-    def __init__(self, path: str):
+    def get_file(self, object_name:str, bucket_name:str):
+        response = None
+        try:
+            response = self.client_instance.get_object(
+                bucket_name=bucket_name, 
+                object_name=object_name
+            )
+        finally:
+            if response != None:
+                response.close()
+                response.release_conn()
+     
+    def upload_file(self,bucket_name,file):
 
-        self.storage_path = path
-
-        if not os.path.isdir(path):
-            os.mkdir(path)
-
-    def upload_file(self,file_to_upload:UploadFile):
-        
-        file_name = file_to_upload.filename
-        final_file_path = os.path.join(self.storage_path,file_name)
-        
-        with open(final_file_path,'wb') as output_file:
-            
-            shutil.copyfileobj(file_to_upload.file,output_file)
-            
-
-        
+        assert self.client_instance != None, "minio client instance should be instantiated first by calling get_instance() class method"
+        self.client_instance.put_object(
+            bucket_name=bucket_name,
+            object_name=file.filename,
+            data=file.file.read()
+        )
